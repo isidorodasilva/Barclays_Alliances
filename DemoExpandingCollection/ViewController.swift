65b0bd948b9e196e5e3e703d0fbafac2ad52ae75
Isidoro Pedro da Silva Junior <silva.11isidoro@gmail.com>
2019-05-21 19:21:26 +0200

    Commit

--- DemoExpandingCollection/ViewController.swift
+++ DemoExpandingCollection/ViewController.swift
@@ -35,19 +35,19 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         return 4
 }
 
-    private func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.contentView.layer.cornerRadius = 4.0
-        cell.contentView.layer.borderWidth = 1.0
-        cell.contentView.layer.borderColor = UIColor.clear.cgColor
-        cell.contentView.layer.masksToBounds = false
-        cell.layer.shadowColor = UIColor.white.cgColor
-        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
-        cell.layer.shadowRadius = 4.0
-        cell.layer.shadowOpacity = 1.0
-        cell.layer.masksToBounds = false
-        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
-        return cell
-        }
+//    private func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+//        cell.contentView.layer.cornerRadius = 4.0
+//        cell.contentView.layer.borderWidth = 1.0
+//        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+//        cell.contentView.layer.masksToBounds = false
+//        cell.layer.shadowColor = UIColor.white.cgColor
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+//        cell.layer.shadowRadius = 4.0
+//        cell.layer.shadowOpacity = 1.0
+//        cell.layer.masksToBounds = false
+//        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+//        return cell
+//        }
 }
